a3606d609dab159f8d4935490720dac43e177c62
KuEmTrinh <vodaitrinh010696@gmail.com>
2022-05-28 23:56:06 +0900

    update

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -7,6 +7,8 @@ import { firebase } from "../../app/firebase";
 import { useDispatch } from "react-redux";
 import { setUserInfomation } from "./loginSlice";
 import Home from "../home/Home";
+import { db } from "../../app/firebase";
+import { Dns } from "@mui/icons-material";
 export default function Login() {
   const [isUserSignedIn, setIsUserSignedIn] = useState(false);
   const dispatch = useDispatch();
@@ -20,11 +22,40 @@ export default function Login() {
       return setIsUserSignedIn(false);
     });
   }, []);
+  const checkUser = (user) => {
+    const userId = user.uid;
+    console.log(userId);
+    let checkUser = false;
+    const fetchUser = async () => {
+      const data = await db
+        .collection("user")
+        .get()
+        .then((snapshot) => {
+          snapshot.docs.map((el) => {
+            console.log(el.id);
+            if (el.id == userId) {
+              console.log("tim thay");
+              checkUser = true;
+            }
+          });
+        });
+    };
+    console.log(checkUser);
+    if (checkUser) {
+      console.log("User data created");
+    } else {
+      db.collection("user").doc(userId).set({
+        name: user.displayName,
+      });
+    }
+    fetchUser();
+  };
   const loginWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(authentication, provider)
       .then((res) => {
-        console.log("Login Success");
+        checkUser(res.user);
+        console.log("Da dang nhap");
       })
       .catch((error) => {
         console.log("Login Failed");
